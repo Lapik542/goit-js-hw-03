84dde98aaee049e16c937e1b348751522c84b6b2
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-10-26 20:14:32 +0300

    Update script-1.js

--- script-1.js
+++ script-1.js
@@ -6,3 +6,5 @@ console.log(slugify("Arrays for begginers"));
 console.log(slugify("English for developer"));
 console.log(slugify("Ten secrets of JavaScript"));
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+
