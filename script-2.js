cd6dd6c9e2cbaa2da8e11ec1e3832daaac5b81b5
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-10-31 12:22:14 +0200

    main

--- script-2.js
+++ script-2.js
@@ -1,14 +1,14 @@
-// function makeArray(firstArray, secondArray, maxLength) {
-//    const newMakeArray = firstArray.concat(secondArray);
-//    if (newMakeArray.length > maxLength) {
-//       return newMakeArray.slice(newMakeArray, maxLength)
-//    } else {
-//       return newMakeArray
-//    }
-// }
-// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
-// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
-// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
-// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+function makeArray(firstArray, secondArray, maxLength) {
+   const newMakeArray = firstArray.concat(secondArray);
+   if (newMakeArray.length > maxLength) {
+      return newMakeArray.slice(newMakeArray, maxLength)
+   } else {
+      return newMakeArray
+   }
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
--- script-3.js
+++ script-3.js
@@ -1,6 +1,11 @@
 function filterArray(numbers, value) {
-   for (let i = value; numbers > i; i++) {
-   }
+  const filteredArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredArray.push(numbers[i]);
+    }
+  }
+  return filteredArray;
 }
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
